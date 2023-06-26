2b2a4219d88c953bec75fcaca21da4cf6a6826e7
HauMinh <58554859+HauMinh@users.noreply.github.com>
2023-06-26 18:17:17 +0700

    show data list users

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.scss';
 import Header from './components/Header';
 import TableUsers from './components/TableUser';
+import Container from 'react-bootstrap/Container';
 
 
 function App() {
   return (
     <div className='app-container'>
       <Header/>
-      <TableUsers/>
+      <Container>
+        <TableUsers/>
+      </Container>
+
     </div>
   );
 }
