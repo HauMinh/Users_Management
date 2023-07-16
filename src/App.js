232c27693458873c4cf93466a6925909473f1ff9
HauMinh <58554859+HauMinh@users.noreply.github.com>
2023-07-16 19:39:22 +0700

    create UI add new user

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import Header from './components/Header';
 import TableUsers from './components/TableUser';
 import Container from 'react-bootstrap/Container';
 import ModelAddNewUser from './components/ModelAddNewUser';
+import { useState } from 'react';
 
 
 function App() {
+  const [isShowModelAddNew, setisShowModelAddNew]= useState(false);
+  const handleClose = ()=>{
+    setisShowModelAddNew(false);
+  }
 
   return (
     <div className='app-container'>
@@ -13,15 +18,19 @@ function App() {
       <Container>
         <div className="my-3 add-new">
           <span><b>list users:</b> </span>
-          <button className="btn btn-success">Add new user</button>
+          <button className="btn btn-success" onClick={()=>setisShowModelAddNew(true)}>Add new user</button>
         </div>
         <TableUsers/>
       </Container>
 
-      <ModelAddNewUser/>
+      <ModelAddNewUser
+        show={isShowModelAddNew}
+        handleClose={handleClose}
+      />
 
     </div>
   );
 }
 
 export default App;
+
--- src/components/ModelAddNewUser.js
+++ src/components/ModelAddNewUser.js
@@ -1,29 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-const ModelAddNewUser = () => {
+const ModelAddNewUser = (props) => {
+    const {show, handleClose} = props;
+    const [name, setName]= useState("");
+    const [job, setJob] = useState("");
+
+
+    const onHandleSaveUser = ()=>{
+        console.log("name", name)
+        console.log("job", job)
+    }
     return (
         <div
         className="modal show"
         style={{ display: 'block', position: 'initial' }}
         >
-        <Modal.Dialog>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal title</Modal.Title>
-          </Modal.Header>
-  
-          <Modal.Body>
-            <p>Modal body text goes here.</p>
-          </Modal.Body>
-  
-          <Modal.Footer>
-            <Button variant="secondary">Close</Button>
-            <Button variant="primary">Save changes</Button>
-          </Modal.Footer>
-        </Modal.Dialog>
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add new user</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='body-add-new'>
+          <form>
+            <div className="form-group">
+              <label>Name</label>
+              <input 
+                type="text" 
+                className="form-control"  
+                placeholder="Name" 
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Job</label>
+              <input 
+                type="text" 
+                className="form-control" 
+                placeholder="Job" 
+                value={job}
+                onChange={(event) => setJob(event.target.value)}
+              />
+            </div>
+          </form>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={onHandleSaveUser}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
       </div>
     );
 };
 
 export default ModelAddNewUser;
+
+
